b0e834283558d73b5c8f84e0db90fc8e3ad74fc5
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2022-10-30 17:31:40 +0500

    fix new design step 2

--- js/main.js
+++ js/main.js
@@ -220,23 +220,7 @@
         }
         
 
-<<<<<<< Updated upstream
         $(".subheader__catalog, .catalogmenu").on({
-=======
-        if($('.banner-main__wrap').length > 0) {
-            $('.banner-main__wrap').slick({
-                infinite: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                arrows: false,
-                autoplay: true,
-                autoplaySpeed: 2000
-            });
-        }
-
-        $(".menu-catalog-link, .catalogmenu").on({
->>>>>>> Stashed changes
             mouseenter: function () {
                 $(".catalogmenu").css("display", "block");
             },
@@ -245,24 +229,15 @@
             }
         });
 
-        $('._minus').click(function(){
-            let counter = $(this).next().val();
-            counter--;
-            if (counter <= 0 ) {
-                $(this).next().val(0);
-            } else {
-                $(this).next().val(counter);
-            }
-            console.log(counter);
-        })
-        $('._plus').click(function(){
-            let counter = $(this).prev().val();
-            counter++;
-            $(this).prev().val(counter);
-            console.log(counter);
-        })
-
+        // FAQ accordion
         $('.faq__item-head').click(function() {
+            $(".faq__item-body").not($(this).next()).slideUp(400);
+            $(this).next().slideToggle(400);
+        
+            $(".faq__item").not($(this).closest(".faq__item")).removeClass("open-accordion");
+            $(this).closest(".faq__item").toggleClass("open-accordion");
+        });
+        $('.faq__item-body').click(function() {
         
             $(".faq__item-body").not($(this).next()).slideUp(400);
             $(this).next().slideToggle(400);
@@ -271,9 +246,98 @@
             $(this).closest(".faq__item").toggleClass("open-accordion");
         });
 
+        // Link to cart
+
+        $('.catalog-item__action--link').click(function(e) {
+        });
+        // $('.catalog-item__in-quanity').click(function(e) {
+        //     console.log(this.closest('a'));
+        //     this.closest('a').bind('click', false)
+        //     console.log(e.parents('.catalog-item__in-quanity').length);
+        //     if (this.closest('a').hasClass('catalog-item__in-quanity')){
+        //         console.log('123');
+        //     }
+        //     });
+
         $('.cart-item__price').toLocaleString('ru');
 
-        console.log(parseInt($('.cart-item__price').text()));
+        // console.log(parseInt($('.cart-item__price').text()));
 
     });
 })(jQuery);
+
+
+//disable link to cart on counter
+
+let buttonCart = document.querySelectorAll('.catalog-item__action--link');
+
+buttonCart.forEach( function (e) {
+    e.addEventListener('click', (element) => {
+        if (element.target.classList.contains('catalog-item__in-title') || element.target.classList.contains('catalog-item__in')) {
+        }  else {
+            element.preventDefault();
+        }
+    })
+})
+
+// add to cart button
+
+let catalogItemButton = document.querySelectorAll('.catalog-item__action');
+
+catalogItemButton.forEach( function (element) {
+    element.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (!element.classList.contains('catalog-item__action--link') && !e.target.classList.contains('_minus')) {
+            element.classList.add('catalog-item__action--link');
+            element.innerHTML = `<div class="catalog-item__in">
+            <div class="catalog-item__in-title">В корзине</div>
+            <div class="catalog-item__in-quanity">
+                <span class="_minus">
+                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.46967 5.46967C0.176777 5.76256 0.176777 6.23744 0.46967 6.53033L5.24264 11.3033C5.53553 11.5962 6.01041 11.5962 6.3033 11.3033C6.59619 11.0104 6.59619 10.5355 6.3033 10.2426L2.06066 6L6.3033 1.75736C6.59619 1.46447 6.59619 0.989592 6.3033 0.696699C6.01041 0.403805 5.53553 0.403805 5.24264 0.696699L0.46967 5.46967ZM2 5.25L1 5.25L1 6.75L2 6.75L2 5.25Z" fill="white"/>
+                    </svg>
+                </span>
+                <input type="text" name="quanity" value="1">
+                <span class="_plus">
+                    <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.53033 5.46967C6.82322 5.76256 6.82322 6.23744 6.53033 6.53033L1.75736 11.3033C1.46447 11.5962 0.989592 11.5962 0.696699 11.3033C0.403805 11.0104 0.403805 10.5355 0.696699 10.2426L4.93934 6L0.6967 1.75736C0.403806 1.46447 0.403806 0.989592 0.6967 0.696699C0.989593 0.403805 1.46447 0.403805 1.75736 0.696699L6.53033 5.46967ZM5 5.25L6 5.25L6 6.75L5 6.75L5 5.25Z" fill="white"/>
+                    </svg>
+                    </span>
+            </div>
+            </div>`;
+            changeCounterCart();
+        }
+    })
+})
+
+// change counter add to cart
+
+function changeCounterCart() {
+    let counterMinusButton = document.querySelectorAll('._minus');
+    let counterPlusButton = document.querySelectorAll('._plus');
+    
+    let cartCounter = 1;
+    
+    counterMinusButton.forEach( function (element) {
+        element.addEventListener('click', (e) => {
+            cartCounter--;
+            element.nextElementSibling.value = cartCounter;
+            if (cartCounter <=0) {
+                
+                cartCounter = 0;
+                element.nextElementSibling.value = cartCounter;
+
+                e.target.closest('.catalog-item__action').classList.remove('catalog-item__action--link');
+                e.target.closest('.catalog-item__action').innerHTML = `<div class="catalog-item__button">Добавить в корзину</div>`;
+            }
+        })
+    })
+    counterPlusButton.forEach( function (element) {
+        element.addEventListener('click', () => {
+            cartCounter++;
+            element.previousElementSibling.value = cartCounter;
+        })
+    })
+}
+
+changeCounterCart();
